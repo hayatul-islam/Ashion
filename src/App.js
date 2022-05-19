232b3cca23a292e352cc0e1a9196272e59d23c2c
hayatul-islam <hayatulemon340@gmail.com>
2022-05-19 16:07:44 +0600

    top banner design

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Conponents/Header/Header';
 import Products from './Conponents/Products/Products';
+import TopBanner from './Conponents/TopBanner/TopBanner';
 
 function App() {
   return (
     <div>
       <Header />
+      <TopBanner />
       <Products />
     </div>
   );
---
+++ src/Conponents/TopBanner/TopBanner.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Col, NavLink, Row } from 'react-bootstrap';
+import './TopBanner.css';
+
+const TopBanner = () => {
+    return (
+        <div>
+            <Row className='py-2'>
+                <Col md={6}>
+                    <div className='women px-5'>
+                        <div>
+                            <h2>Women's Fashion</h2>
+                            <p className='fs-5 pt-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi eum eligendi blanditiis doloremque?</p>
+                            <NavLink className='text-uppercase text-black ps-0 fs-5'>Shop Now</NavLink>
+                            <div>
+                                <hr style={{ width: '104px', padding: '1.6px 0', marginTop: '-5px', background: 'red', }} />
+                            </div>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={6}>
+                    <Row>
+                        <Col md={6} className="px-0 pb-2">
+                            <div className='mens fashion px-5'>
+                                <div>
+                                    <h2>Men's Fashion</h2>
+                                    <p className='fs-5 pt-0'>383 items</p>
+                                    <NavLink className='text-uppercase text-black ps-0 fs-5'>Shop Now</NavLink>
+                                    <div>
+                                        <hr style={{ width: '104px', padding: '1.6px 0', marginTop: '-5px', background: 'red', }} />
+                                    </div>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col md={6} className="pb-2">
+                            <div className='kids fashion px-5'>
+                                <div>
+                                    <h2>Kid's Fashion</h2>
+                                    <p className='fs-5 pt-0'>383 items</p>
+                                    <NavLink className='text-uppercase text-black ps-0 fs-5'>Shop Now</NavLink>
+                                    <div>
+                                        <hr style={{ width: '104px', padding: '1.6px 0', marginTop: '-5px', background: 'red', }} />
+                                    </div>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col md={6} className="px-0 pb-2">
+                            <div className='cosmetics fashion px-5'>
+                                <div>
+                                    <h2>Cosmetics</h2>
+                                    <p className='fs-5 pt-0'>383 items</p>
+                                    <NavLink className='text-uppercase text-black ps-0 fs-5'>Shop Now</NavLink>
+                                    <div>
+                                        <hr style={{ width: '104px', padding: '1.6px 0', marginTop: '-5px', background: 'red', }} />
+                                    </div>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col md={6}>
+                            <div className='accessories fashion px-5'>
+                                <div>
+                                    <h2>Accessories</h2>
+                                    <p className='fs-5 pt-0'>383 items</p>
+                                    <NavLink className='text-uppercase text-black ps-0 fs-5'>Shop Now</NavLink>
+                                    <div>
+                                        <hr style={{ width: '104px', padding: '1.6px 0', marginTop: '-5px', background: 'red', }} />
+                                    </div>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+export default TopBanner;
