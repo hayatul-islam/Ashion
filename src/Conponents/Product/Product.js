819189e7f1a5d98e3e16b6ffc9df8658a43873c2
hayatul-islam <hayatulemon340@gmail.com>
2022-05-19 13:11:08 +0600

    rating dynamic

--- src/Conponents/Product/Product.js
+++ src/Conponents/Product/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Col } from 'react-bootstrap';
+import StarRatings from 'react-star-ratings/build/star-ratings';
 
 const Product = ({ product }) => {
     const { title, image, price, rating } = product;
@@ -8,9 +9,14 @@ const Product = ({ product }) => {
             <Card className='mb-3'>
                 <Card.Img style={{ height: '400px' }} variant="top" src={image} />
                 <Card.Body className='text-center'>
-                    <Card.Title>{title.slice(0, 20)}..</Card.Title>
-                    <Card.Text>{rating?.rate}</Card.Text>
-                    <Card.Text>${price}</Card.Text>
+                    <Card.Title className='fs-4 mb-3'>{title.slice(0, 20)}..</Card.Title>
+                    <Card.Text> <StarRatings
+                        rating={rating?.rate}
+                        starDimension="24px"
+                        starSpacing="5px"
+                        starRatedColor="orange"
+                    /> </Card.Text>
+                    <Card.Text className='fs-4 fw-bold text-dark'>$ {price}</Card.Text>
                 </Card.Body>
             </Card>
         </Col>
